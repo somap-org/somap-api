14a0de010a34851e542c4090735e6754f93217bc
Luca Pirrone <lpirrone2000@gmail.com>
2020-12-28 23:19:34 +0100

    modifica dell'attributo ora è una promessa

--- src/services/userGraph/user/signUp.ts
+++ src/services/userGraph/user/signUp.ts
@@ -77,11 +77,8 @@ export async function main(event) {
                 Username: event.request.userAttributes.sub
             };
             console.log('params', params);
-            cognitoIdentityServiceProvider.adminUpdateUserAttributes(params, function(err, data) {
-                if (err) console.log(err, err.stack); // an error occurred
-                else     console.log(data);           // successful response
-                return userAdded;
-            });
+            await cognitoIdentityServiceProvider.adminUpdateUserAttributes(params).promise();
+            return userAdded;
         } catch (e) {
             console.log('ERROR ADDING USER', e);
             return null;
