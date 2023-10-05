0ccd7d241b4de9bc8df3399af058ced686ad0eb0
Luca Pirrone <lpirrone2000@gmail.com>
2023-10-05 18:47:27 +0200

    ivs type

--- src/services/userGraph/user/signUp.ts
+++ src/services/userGraph/user/signUp.ts
@@ -75,7 +75,7 @@ export async function main(event) {
         const params = {
           latencyMode: 'LOW',
           name: userAdded['_id'].toString(),
-          type: 'STANDARD'
+          type: 'ADVANCED_SD'
         };
         const ivsResult = await ivs.createChannel(params).promise();
         let live = await repo.updateLiveInfo(userAdded._id, {
